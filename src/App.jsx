a4b7faebb7c7292f65c563d7dafa2a25bc2f2a51
zakiibnu723 <123972239+zakiibnu723@users.noreply.github.com>
2025-01-04 10:49:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { GlassesARView } from '@lastcode802/glassarview';
+import * as React from 'react';
+import { GlassArView } from "@lastcode802/glassarview";
 
 function App() {
   return (
-    <GlassesARView
-      // model3dmtl="./assets/glassesModel.mtl"
-      model3dobj="./assets/MOSCOT_ZEV_TT_SE.obj"
-      // Additional props and configurations
+    <GlassArView
+      modelname="rayban_aviator_or_vertFlash"
+      canvasheight={500}
+      canvaswidth={500}
+      buttonBackgroundColor="#FFE5B4"
+      buttonFontColor="white"
     />
   );
 }
